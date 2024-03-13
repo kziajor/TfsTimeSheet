288117c4dbe2117c4b20e4cdec22307310479802
Krzysztof Ziajor <krzysztof.ziajor@gmail.com>
2024-03-13 19:43:57 +0100

    Add dependency injection

--- App/Program.cs
+++ App/Program.cs
@@ -1,18 +1,45 @@
 using Honest.Timesheet.DbMigrator;
+using Honest.Timesheet.Domain.Interfaces;
+using Honest.Timesheet.Persistence;
+using Honest.Timesheet.Persistence.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace App
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
             ApplicationConfiguration.Initialize();
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+
+            var host = CreateHostBuilder().Build();
+
             DbMigrator.Migrate("Data Source=.\\Data.db");
-            Application.Run(new Main());
+            Application.Run(host.Services.GetRequiredService<Main>());
+        }
+
+        private static IHostBuilder CreateHostBuilder()
+        {
+            return Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddDbContext<AppDbContext>();
+                    services.RegisterForms();
+                    services.RegisterRepositories();
+                });
+        }
+
+        private static void RegisterForms(this IServiceCollection services)
+        {
+            services.AddTransient<Main>();
+        }
+
+        private static void RegisterRepositories(this IServiceCollection services)
+        {
+            services.AddTransient<IProjectRepository, ProjectRepository>();
         }
     }
 }
